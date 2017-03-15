9ece6f27995015bb6a9c172efa614e7a5fb574fe
dev <allgame789@gmail.com>
2017-03-15 17:25:12 +0700

    update code

--- app/src/components/Partials/Header.js
+++ app/src/components/Partials/Header.js
@@ -14,22 +14,18 @@ class Header extends React.Component {
 								<a href="#">Line of me</a>
 							</h1>
 							<div id="tool-filter">
-								<div className="tool-wrap-icon" >
-									<a href="javascript:void(0)" class="uk-icon" uk-icon="icon: cog; ratio: 1.2"></a>
-									<div uk-dropdown="mode: click; pos: bottom-right">
-										<ul className="uk-nav uk-dropdown-nav">
-											<li className="uk-active"><a href="#">Active</a></li>
-											<li><a href="#">Item</a></li>
-											<li className="uk-nav-header">Header</li>
-											<li><a href="#">Item</a></li>
-											<li><a href="#">Item</a></li>
-											<li className="uk-nav-divider"></li>
-											<li><a href="#">Item</a></li>
-										</ul>
+								<div className="tool-wrap-icon">
+									<a class="uk-icon" uk-icon="icon: commenting; ratio: 1.2" href="#modal-container" uk-toggle></a>		
+									<div id="modal-container" class="uk-modal-container" uk-modal>
+											<div class="uk-modal-dialog uk-modal-body">
+													<button class="uk-modal-close-default" type="button" uk-close></button>
+													<h2 class="uk-modal-title">Headline</h2>
+													<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+											</div>
 									</div>
 								</div>
 								<div className="tool-wrap-icon">
-									<a href="javascript:void(0)" class="uk-icon" uk-icon="icon: menu; ratio: 1.5"></a>
+									<a href="javascript:void(0)" class="uk-icon" uk-icon="icon: thumbnails; ratio: 1.45"></a>
 									<div uk-dropdown="mode: click; pos: bottom-right">
 										<ul className="uk-nav uk-dropdown-nav">
 											<li className="uk-active"><a href="#">Active</a></li>
